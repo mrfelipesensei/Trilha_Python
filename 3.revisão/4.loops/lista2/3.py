bc8eed993e1a34a8270a141c0ac8d390637cdfb0
Felipe Greg <mrfelipesensei@gmail.com>
2025-02-01 13:32:31 -0300

    loops lista2 ex2 - update

--- 3.revisão/4.loops/lista2/3.py
+++ 3.revisão/4.loops/lista2/3.py
@@ -7,15 +7,25 @@ while num != sorteio and tentativas < 3:
     try:
         num = int(input("Digite um número: "))
         tentativas+=1 #incremento
+        tentativas_restantes = 3 - tentativas #calcula as tentativas restantes
+
         if 1 <= num <= 10: #testando o palpite
             if num > sorteio:
                 print("É menor")
+                if tentativas < 3: #Informa as tentativas restantes
+                    print(f"Você tem mais {tentativas_restantes} tentativa(s).")
             elif num < sorteio:
                 print("É maior")
-
+                if tentativas < 3: #Informa as tentativas restantes
+                    print(f"Você tem mais {tentativas_restantes} tentativa(s).")
+        
         if num == sorteio:
             print("Você ACERTOU!")
             break
     
     except ValueError: #Tratamento de erros
         print("Entrada Inválida. Digite um número inteiro. ")
+
+if num != sorteio and tentativas == 3:
+    print("Você não acertou.")
+    print("Número de tentativas encerrado.")
