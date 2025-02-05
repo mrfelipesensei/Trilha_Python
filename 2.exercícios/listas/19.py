ef44b105e22a7f66b0ff5f2974374a053faa84ed
Felipe Greg <mrfelipesensei@gmail.com>
2025-02-05 17:00:39 -0300

    listas ex19 - tratamento de erros

--- 2.exercícios/listas/19.py
+++ 2.exercícios/listas/19.py
@@ -3,9 +3,15 @@ nomes = []
 
 for i in range(5):
     while True:
-        nome = input(f"Digite o {i+1}º nome: ")
-        nomes.append(nome)
-        break
+        try:
+            nome = input(f"Digite o {i+1}º nome: ")
+            if nome.isalpha(): #Verifica se a string contém apenas letras
+                nomes.append(nome)
+                break
+            else:
+                raise ValueError("Entrada inválida. Digite apenas letras.")
+        except ValueError as e:
+            print(e)
 
 #Ordena a lista de nomes em ordem alfabética
 nomes.sort()
